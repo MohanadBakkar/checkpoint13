9dace15f1cc7c9ce80dbe2292077b94397cc757e
rodwan-bakkar <rodwan.bakkar@gmail.com>
2024-02-11 21:07:35 +0100

    add video trailer

---
+++ src/Description.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link, useParams } from 'react-router-dom';
+
+const Description = ({ movies }) => {
+    const params = useParams();
+    console.log(params.movieId);
+    const movie = movies.find(movie => movie.movieId === parseInt(params.movieId));
+  
+    return (
+      <div>
+        <div>{movie.description}</div>
+        <iframe width="600" height="300" autoplay= "1" />
+        <div>
+        <Link to="/">Back Home</Link>
+        </div>
+      </div>
+    );
+  };
+
+  export default Description
